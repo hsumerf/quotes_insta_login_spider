1d8452e9659b053f57077ec595c64c7b7a94814e
H S Umer Farooq <h.s.umer.farooq@gmail.com>
2018-07-05 03:39:57 +0500

    after copy code from youtube vid

--- quotes_login_spider/spiders/insta_login.py
+++ quotes_login_spider/spiders/insta_login.py
@@ -2,7 +2,9 @@
 import scrapy
 from scrapy.http import FormRequest
 from scrapy.utils.response import open_in_browser
-
+import requests
+import json
+import getpass
 
 class InstaLoginSpider(scrapy.Spider):
     name = 'insta_login'
@@ -12,6 +14,28 @@ class InstaLoginSpider(scrapy.Spider):
     def parse(self, response):
         print(response.url)
         csrf_token = response.xpath('//*[@name="csrf_token"]/@value').extract_first()
+        BASE_URL = 'http://www.instagram.com'
+        LOGIN_URL = BASE_URL + '/accounts/login/'
+        USER_NAME = 'h.s.umer.farooq@gmail.com'
+        PASSWD = 'umer+-789'
+        USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+        session = requests.Session()
+        session.headers = {'user-agent':USER_AGENT}
+        session.headers.update({'Referer':BASE_URL})
+        req = session.get(BASE_URL)
+
+        session.headers.update({'X-CSRFToken':req.cookies['csrftoken']})
+        login_data = {'username':USER_NAME,'password':PASSWD}
+        login = session.post(LOGIN_URL,data=login_data,allow_redirects = True)
+        session.headers.update({'X-CSRFToken':login.cookies['csrftoken']})
+
+        cookies = login.cookies
+        print(cookies)
+
+
+        print(login)
+        print(login.url)
+        # login_text = json.loads(login.text)
         # yield FormRequest('http://quotes.toscrape.com/login',
         #                   formdata={'csrf_token': csrf_token,
         #                             'username': 'h.s.umer.farooq@gmail.com',
